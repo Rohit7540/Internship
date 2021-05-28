8d76fd2ca5fb4e1c6281bee7b7dfa415ed70a487
Rohit Makwana <84960405+Rohit7540@users.noreply.github.com>
2021-05-28 15:11:37 +0530

    Delete company directory

--- company/day 2/dictionary data type.py
+++
@@ -1,5 +0,0 @@
-d = {1:'makwana',2:'Rohit','key':20}
-print(type(d))
-print("d[1]= ",d[1])
-print("d[2]= ",d[2])
-print("d['key']= ",d['key'])
--- company/day 2/list data type.py
+++
@@ -1,5 +0,0 @@
-list = [10,20,30,40,"rohit"]
-print(list)
-print("list[3]=",list[3])
-print("list[0:2]=",list[0:2])
-print("list[3:]=",list[3:])
