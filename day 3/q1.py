ad8048f86f02a40a226266be9e34e276891ad5f7
Rohit Makwana <84960405+Rohit7540@users.noreply.github.com>
2021-05-28 15:14:37 +0530

    day 3
    python program exercise.

---
+++ day 3/q1.py
@@ -0,0 +1,11 @@
+num1= int(input("enter first number :"))
+num2= int(input("enter second number :"))
+num3= int(input("enter third number :"))
+num4= int(input("enter four number :"))
+num5= int(input("enter five number :"))
+
+avg=num1+num2+num3+num4+num5/5
+
+print("avg of number : ",avg)
+
+
