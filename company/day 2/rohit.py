2b18195c975e7b0f85f3078afac9bdfc768af114
Rohit Makwana <84960405+Rohit7540@users.noreply.github.com>
2021-05-28 15:06:13 +0530

    day 2
    working with datatype,list,dictionary,tuple

---
+++ company/day 2/rohit.py
@@ -0,0 +1,16 @@
+print("Hello World")
+# Variable store
+a = 30
+b = 10
+c = "rohit"
+name = "rohit"
+print("Name is",name)
+#assiging multiple
+a, b, c = 15,20,"rohit"
+print(a)
+print(b)
+print(c)
+a, b, c = 10,50,"rohit"
+print("value is : ",a)
+print("value is : ",b)
+print("value is : ",c)
---
+++ company/day 2/tuple data type.py
@@ -0,0 +1,9 @@
+tuple = (11,22,33,44,"Rohit")
+print(tuple)
+print("tuple[2]=",tuple[4])
+print("tuple[0:3]=",tuple[0:2])
+print("tuple[2:]=",tuple[2:])
+print("tuple[:3]=",tuple[:4])
+
+# no change in tuple error
+tuple[1] = 15
