49ed01dad5da398be65adc1e0de8f533ca7444b5
Rohit Makwana <84960405+Rohit7540@users.noreply.github.com>
2021-05-28 15:13:19 +0530

    day 2
    working with datatypes.

---
+++ day 2/data type.py
@@ -0,0 +1,13 @@
+n1 = 40
+print(n1)
+print(type(n1))
+
+n2 = 45.5
+print(n2)
+print(type(n2))
+
+n3 = 4 + 2j
+print(n3)
+print(type(n3))
+print(n3,"is complex?", isinstance(4 + 2j,complex))
+
---
+++ day 2/list data type.py
@@ -0,0 +1,5 @@
+list = [10,20,30,40,"rohit"]
+print(list)
+print("list[3]=",list[3])
+print("list[0:2]=",list[0:2])
+print("list[3:]=",list[3:])
